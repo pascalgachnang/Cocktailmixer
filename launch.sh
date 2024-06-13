0fa85f197c39a484a0160f4b938c8e9c72cc9044
Pascal Gachnang <p.gachnang@gmx.ch>
2024-06-13 18:55:05 +0200

    launch script enhanced to switch to virtual environment

--- launch.sh
+++ launch.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+# Activate the virtual environment
+
+which python
+
 cd /home/pi/Cocktailmixer
+pwd
+activate() {
+    . /home/pi/Cocktailmixer/mixingjenny-env/bin/activate
+}
+activate
+which python
+
+# Run the main script
 python ./src/main.py
